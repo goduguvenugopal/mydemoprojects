a134755cd408b0911978ac7ddac50a57127dfa42
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2023-12-01 13:21:41 +0530

    tourism website

--- venu.js
+++ venu.js
@@ -1,16 +1,1 @@
- 
-
-function change(){
-    document.body.style.backgroundColor = "pink";
-}
- 
-document.getElementById("vkk").innerHTML = "venugopal";
-function venu(){
-    document.getElementById("cont").style.backgroundColor = "red";
-    
-}
-
-function venu1(){
-     
-   document.getElementById("cont").style.backgroundColor = "green";
-}
+  
