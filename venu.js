9161901dd98e81e649139d7104b835a788ad2a5e
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2023-12-13 15:00:15 +0530

    search bar updated

--- venu.js
+++ venu.js
@@ -1,17 +1,1 @@
-  function callbackfunction1(){
-    console.log("hii this is venu gopal i want to become a software1 developer by the 2026.")
-  }
-
-  function callbackfunction2(){
-    console.log("hii this is venu gopal i want to become a software2 developer by the 2026.")
-  }
-
-  function add(n1, n2,a){
-    console.log(n1-n2)
-    a()
-  }
-
-  let a = 144
-  let b = 6
-
-  add(a,b,callbackfunction2)
+  fu
