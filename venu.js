0ad6b1bc0f14fac6f81da91d2d6ed2ad18b8e19f
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2023-12-23 16:14:44 +0530

    add

--- venu.js
+++ venu.js
@@ -2,8 +2,44 @@
 
   
  
-   let name = prompt("please enter your name")
+  
+  
+       function myfunction(){
+  
+      let name = prompt("Please Enter Your Name")
+
+      let marks = prompt("Please Enter Your Marks")
+      let result;
+      switch(true){
+      case marks > 90 && marks <=100 :
+      result = "great you are the topper"
+      break;
+      case marks >65 && marks <=90 :
+        result = "congratulations you got first class"
+        break;
+        case marks >=40 && marks <=65 :
+          result = "you got second class"
+          break;
+          case marks >=30 && marks <=40:
+            result = "you got third class"
+            break;
+            case marks <30 :
+             result = "sorry you are failed , better luck next time"
+           break;
+             
+             default:
+           result = "please enter your marks"
+            break;
+            } 
  
+            
+ 
+      document.getElementById("demo").innerHTML = "Hello " + name + " " + result + " ";
+       
+      
+
+
+  }
   
  
 
